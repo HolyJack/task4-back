0d2ec9a50f3084f05a9f5b1cedc27219e6f28d3f
holyjack <holyjackdev@gmail.com>
2023-12-09 19:45:22 +0400

    new build

--- app.js
+++ app.js
@@ -1,42 +1,16 @@
 "use strict";
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator["throw"](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
@@ -51,68 +25,58 @@ const app = (0, express_1.default)();
 const port = 3000;
 const env = (0, dotenv_1.config)();
 app.use(passport_1.default.initialize());
-app.use(
-  (0, express_session_1.default)({
-    secret:
-      ((_a = env.parsed) === null || _a === void 0 ? void 0 : _a.SECRET) ||
-      "dev",
+app.use((0, express_session_1.default)({
+    secret: ((_a = env.parsed) === null || _a === void 0 ? void 0 : _a.SECRET) || "dev",
     resave: false,
     saveUninitialized: false,
     cookie: {
-      httpOnly: false,
+        httpOnly: false,
     },
-  }),
-);
-app.use(
-  (0, cors_1.default)({
-    origin: "http://localhost:5173",
+}));
+app.use((0, cors_1.default)({
+    origin: "task4-front-snowy.vercel.app",
     credentials: true,
-  }),
-);
+}));
 app.use(express_1.default.json());
 app.use(passport_1.default.session());
-passport_1.default.use(
-  new passport_local_1.Strategy((username, password, done) =>
-    __awaiter(void 0, void 0, void 0, function* () {
-      try {
+passport_1.default.use(new passport_local_1.Strategy((username, password, done) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
         const user = yield db_1.default.user.findFirst({
-          where: { username, password },
+            where: { username, password },
         });
         if (!user) {
-          return done(null, false, {
-            message: "Incorrect username or password",
-          });
+            return done(null, false, { message: "Incorrect username or password" });
         }
         if (!user.active) {
-          return done(null, false, {
-            message: "Your account is inactive. Contact support.",
-          });
+            return done(null, false, {
+                message: "Your account is inactive. Contact support.",
+            });
         }
         return done(null, user);
-      } catch (err) {
+    }
+    catch (err) {
         return done(err);
-      }
-    }),
-  ),
-);
+    }
+})));
 // @ts-ignore
 // no overload mathes this call
 passport_1.default.serializeUser((user, done) => {
-  if (user.id) return done(null, user.id);
-  return done(null, false);
+    if (user.id)
+        return done(null, user.id);
+    return done(null, false);
 });
-passport_1.default.deserializeUser((id, done) =>
-  __awaiter(void 0, void 0, void 0, function* () {
+passport_1.default.deserializeUser((id, done) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-      const user = yield db_1.default.user.findFirst({
-        where: { id: id },
-      });
-      if (user) return done(null, user);
-      return done(null, false);
-    } catch (err) {
-      return done(err);
+        const user = yield db_1.default.user.findFirst({
+            where: { id: id },
+        });
+        if (user)
+            return done(null, user);
+        return done(null, false);
     }
-  }),
-);
+    catch (err) {
+        return done(err);
+    }
+}));
 app.use(routes_1.default);
 app.listen(port, () => console.log(`This app is listening on port ${port}`));
--- routes/users.js
+++ routes/users.js
@@ -1,42 +1,16 @@
 "use strict";
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator["throw"](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.users = void 0;
 const express_1 = __importDefault(require("express"));
@@ -44,36 +18,30 @@ const db_1 = __importDefault(require("../utils/db"));
 const authenticate_1 = __importDefault(require("../middlewares/authenticate"));
 const users = express_1.default.Router();
 exports.users = users;
-users.get("/users", authenticate_1.default, (_, res) =>
-  __awaiter(void 0, void 0, void 0, function* () {
+users.get("/users", authenticate_1.default, (_, res) => __awaiter(void 0, void 0, void 0, function* () {
     const users = yield db_1.default.user.findMany({
-      select: {
-        username: true,
-        createdAt: true,
-        signinAt: true,
-        active: true,
-      },
+        select: {
+            username: true,
+            createdAt: true,
+            signinAt: true,
+            active: true,
+        },
     });
     res.send(users);
-  }),
-);
-users.patch("/users", authenticate_1.default, (req, res) =>
-  __awaiter(void 0, void 0, void 0, function* () {
+}));
+users.patch("/users", authenticate_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const usernames = req.body.data.usernames;
     const active = req.body.data.active;
     yield db_1.default.user.updateMany({
-      where: { username: { in: usernames } },
-      data: { active },
+        where: { username: { in: usernames } },
+        data: { active },
     });
     res.status(204).send();
-  }),
-);
-users.delete("/users", authenticate_1.default, (req, res) =>
-  __awaiter(void 0, void 0, void 0, function* () {
+}));
+users.delete("/users", authenticate_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const usernames = req.body.usernames;
     yield db_1.default.user.deleteMany({
-      where: { username: { in: usernames } },
+        where: { username: { in: usernames } },
     });
     res.status(204).send();
-  }),
-);
+}));
